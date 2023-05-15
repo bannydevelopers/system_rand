342942aec4a6d218176e718f5ec1f91190e925e4
Wizdon <software@banny.co.tz>
2023-05-15 10:01:03 +0300

    no hurry in Africa

--- rand/modules/portal/index.php
+++ rand/modules/portal/index.php
@@ -1,2 +1,6 @@
 <?php
-header('Location: ./admin');
+$page = 'index';
+$parts = explode('/',$_SERVER['REQUEST_URI']);
+$req = end($parts);
+if(is_readable(__DIR__."/rand/{$req}.html")) $page = $req;
+include "rand/{$page}.html";
