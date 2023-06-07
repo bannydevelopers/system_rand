51ca2fedcf5da0f13b8ff289bd51227b55088227
bannyDev <software@banny.co.tz>
2023-06-07 20:30:09 +0300

    sales

--- rand/modules/default/index.php
+++ rand/modules/default/index.php
@@ -2,24 +2,38 @@
 
 $db = db::get_connection(storage::init()->system_config->database);
 
+
 $apartment = "SELECT COUNT(apartment_id) FROM apartments";
 $apartment = $db->query($apartment);
 $apartmentC = $apartment->fetchColumn();
+
 
 $staff = "SELECT COUNT(staff_id) FROM staff";
 $staff = $db->query($staff);
 $staffC = $staff->fetchColumn();
 
+
 $booked = "SELECT COUNT(check_id) FROM check_scheduling";
 $booked = $db->query($booked);
 $bookedC = $booked->fetchColumn();
 
+
 $expenses = $db->select('expenses','expenses.expenses_amount,expenses.expenses_date')
-                ->order_by('expenses_date', 'desc')->fetchAll();
-$expenseSum = 0;
+                ->order_by('expenses_date', 'desc')
+                ->fetchAll();
+$expensesSum = 0;
 foreach($expenses as $expense) {
-    $expenseSum += $expense['expenses_amount'];
+    $expensesSum += $expense['expenses_amount'];
 }
+
+
+$orders = $db->select('orders','orders.payment_amount')
+                ->fetchAll();
+$ordersSum = 0;
+foreach($orders as $order) {
+    $ordersSum += $order['payment_amount'];
+}
+
 
 $apartmentG = $db->select('apartments','apartments.apartment_block')
                 ->group_by('apartment_block')->fetchAll();
@@ -30,6 +44,14 @@ $apartmentG = $db->select('apartments','apartments.apartment_block')
 // print_r($apartmentArray);
 // die();
 
-$data = ['apartmentC'=>$apartmentC, 'apartmentG'=>$apartmentG, 'staffC'=>$staffC, 'bookedC'=>$bookedC, 'expenseSum'=>$expenseSum];
+
+$data = [
+    'apartmentC' => $apartmentC, 
+    'apartmentG' => $apartmentG, 
+    'staffC' => $staffC, 
+    'bookedC' => $bookedC, 
+    'expensesSum' => $expensesSum,
+    'ordersSum' => $ordersSum
+];
 
 echo helper::find_template('home', $data);
