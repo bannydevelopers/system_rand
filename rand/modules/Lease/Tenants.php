650c3be9ae2d16ac3fd9a5ded1a0fa0b18319d7e
abigairi <abigailjonathan150@gmail.com>
2023-06-09 20:50:12 +0300

    new t

--- rand/modules/Lease/Tenants.php
+++ rand/modules/Lease/Tenants.php
@@ -10,101 +10,100 @@ $role = $db->select('role','role_id')
 $apartment = $db->select('apartments','apartment_id')
                ->fetch();
 
-if(isset($_POST['tenants_id']) && intval($_POST['tenants_id'])){
-        if($helper->user_can('can_edit_tenants')){
-            $names = explode(' ', addslashes($_POST['full_name']));
-            $fn = $names[0];
-            $ln = end($names);
-            array_shift($names);
-            array_pop($names);
-            $mn = implode(' ', $names);
-            $user = [
-                'first_name' => $fn,
-                'middle_name' => $mn,
-                'last_name' => $ln,
-                'system_role' => $role['role_id'],
-                'phone_number'=>helper::format_phone_number($_POST['phone_number']), //
-                'email'=>helper::format_email($_POST['email']), //
-                
+if(isset($_POST['edit-tenant']) ){
+    if($helper->user_can('can_edit_tenants')){
+        $names = explode(' ', addslashes($_POST['full_name']));
+        $fn = $names[0];
+        $ln = end($names);
+        array_shift($names);
+        array_pop($names);
+        $mn = implode(' ', $names);
+        $user = [
+            'first_name' => $fn,
+            'middle_name' => $mn,
+            'last_name' => $ln,
+            'system_role' => $role['role_id'],
+            'phone_number'=>helper::format_phone_number($_POST['phone_number']), 
+            'email'=>helper::format_email($_POST['email']), 
+        ];
+        $whr ="(email='{$user['email']}' OR phone_number = '{$user['phone_number']}')";
+        $test = $db->select('tenants')
+                ->join('user','user_reference=user_id')
+                ->where($whr)
+                ->and("tenants_id != {$_POST['tenants_id']}")
+                ->fetch();
+        if(!$test){
+            $whr = "user_id IN (SELECT user_reference FROM tenants WHERE tenants_id = '{$_POST['tenants_id']}')";
+            $db->update('user', $user)->where($whr)->commit();
+
+            $tenants = [
+                'passport_number'=>addslashes($_POST['passport_number']), 
+                'resident_adress'=>addcslashes($_POST['resident_adress'])
             ];
-            $whr ="(email='{$user['email']}' OR phone_number = '{$user['phone_number']}')";
-            $test = $db->select('tenants')
-                    ->join('user','user_reference=user_id')
-                    ->where($whr)
-                    ->and("tenants_id != {$_POST['tenants_id']}")
-                    ->fetch();
-            if(!$test){
-                $whr = "user_id IN (SELECT user_reference FROM tenants WHERE tenants_id = '{$_POST['tenants_id']}')";
-                $db->update('user', $user)->where($whr)->commit();
 
+            $db->update('tenants', $tenants)->where(['tenants_id'=>intval($_POST['tenants_id'])])->commit();
+            if(!$db->error()) {
+                $msg = 'Updated successful!';
+                $status = 'success';
+            }
+            else $msg = 'Something went wrong!';
+        }
+        else $msg = 'Some unique information already used by other employee';
+    }
+    else $msg = 'Not enough privilege, sorry';
+}
+
+if(isset($_POST['add-tenant'])){
+    if($helper->user_can('can_add_tenants')){
+        $token = helper::create_hash(time());
+
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $phone_number = isset($_POST['phone_number']) ? $_POST['phone_number'] : '';
+        $full_name = isset($_POST['full_name']) ? $_POST['full_name'] : '';
+        $names = explode(' ', addslashes($full_name));
+        $fn = $names[0];
+        $ln = end($names);
+        array_shift($names);
+        array_pop($names);
+        $mn = implode(' ', $names);
+        $user = [
+            'first_name' => $fn, 
+            'middle_name' => $mn, 
+            'last_name' => $ln, 
+            'system_role' => $role['role_id'], 
+            'status'=>'active', 
+            'phone_number'=> isset($_POST['phone_number'])
+            ? helper::format_phone_number($_POST['phone_number']) : '',
+            'email' => isset($_POST['tenants_email']) 
+            ? helper::format_email($_POST['tenants_email']) : '',
+            'password' => helper::create_hash('tenant123'), 
+            'activation_token' => $token, 
+        ];
+        $test = $db->select('tenants', 'user_reference')
+                ->join('user','user_reference=user_id')
+                ->where(['email' => $user['email']])
+                ->or(['phone_number' => $user['phone_number']])
+                ->fetch();
+        if($test) $msg = 'tenants information exists, try to edit existing one if necessary';
+        else {
+            $user_id = $db->insert('user',$user);
+            if(intval($user_id)){
                 $tenants = [
-                    'passport_number'=>addslashes($_POST['passport_number']), 
-                    'resident_adress'=>addcslashes($_POST['resident_adress'])
+                    'user_reference' => $user_id, 
                 ];
-
-                $db->update('tenants', $tenants)->where(['tenants_id'=>intval($_POST['tenants_id'])])->commit();
-                if(!$db->error()) {
-                    $msg = 'Updated successful!';
+                $k = $db->insert('tenants',$tenants);
+                if($db->error() or !$k) $db->delete('user')->where(['user_id',$user_id])->commit(); // revert changes, tenants issues
+                else {
+                    $msg = 'tenants created'; 
                     $status = 'success';
                 }
-                else $msg = 'Something went wrong!';
             }
-            else $msg = 'Some unique information already used by other employee';
+            else $msg = 'Fatal error occured';
         }
-        else $msg = 'Not enough privilege, sorry';
     }
-    else{
-        if($helper->user_can('can_add_tenants')){
-            $token = helper::create_hash(time());
-            
-
-            $password = isset($_POST['password']) ? $_POST['password'] : '';
-            $email = isset($_POST['email']) ? $_POST['email'] : '';
-            $phone_number = isset($_POST['phone_number']) ? $_POST['phone_number'] : '';
-            $full_name = isset($_POST['full_name']) ? $_POST['full_name'] : '';
-            $names = explode(' ', addslashes($full_name));
-            $fn = $names[0];
-            $ln = end($names);
-            array_shift($names);
-            array_pop($names);
-            $mn = implode(' ', $names);
-            $user = [
-                'first_name' => $fn, 
-                'middle_name' => $mn, 
-                'last_name' => $ln, 
-                'system_role' => $role['role_id'], 
-                'status'=>'active', 
-                'phone_number'=> isset($_POST['phone_number'])
-                ? helper::format_phone_number($_POST['phone_number']) : '',
-                'email' => isset($_POST['tenants_email']) 
-                ? helper::format_email($_POST['tenants_email']) : '',
-                'password' => helper::create_hash('tenant123'), 
-                'activation_token' => $token, 
-            ];
-            $test = $db->select('tenants', 'user_reference')
-                    ->join('user','user_reference=user_id')
-                    // ->where(['email' => $user['email']])
-                    // ->or(['phone_number' => $user['phone_number']])
-                    ->fetch();
-            if($test) $msg = 'tenants information exists, try to edit existing one if necessary';
-            else {
-                $user_id = $db->insert('user',$user);
-                if(intval($user_id)){
-                    $tenants = [
-                        'user_reference' => $user_id, 
-                    ];
-                    $k = $db->insert('tenants',$tenants);
-                    if($db->error() or !$k) $db->delete('user')->where(['user_id',$user_id])->commit(); // revert changes, tenants issues
-                    else {
-                        $msg = 'tenants created'; 
-                        $status = 'success';
-                    }
-                }
-                else $msg = 'Fatal error occured';
-            }
-        }
-        else $msg = 'Not enough privilege, sorry';
-    }
+    else $msg = 'Not enough privilege, sorry';
+}
 
 
 $roles = $db->select('role','role_id,role_name')
