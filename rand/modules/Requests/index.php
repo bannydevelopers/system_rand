8532200dc4735492b23497bb62a813d665d955d1
Jusper Wendo <jasperrupia@gmail.com>
2023-06-15 17:02:23 +0300

    hi2

--- rand/modules/Requests/index.php
+++ rand/modules/Requests/index.php
@@ -3,6 +3,7 @@
 $db = db::get_connection(storage::init()->system_config->database);
 $ok = false;
 $msg = '';
+$my = helper::init()->get_session_user();
 
 $request = $_SERVER['REQUEST_URI'];
 if(isset($_POST['house_details'])){
@@ -10,12 +11,12 @@ if(isset($_POST['house_details'])){
         'house_details'=>$_POST['house_details'], 
         'descriptions'=>$_POST['descriptions'], 
         'submitted_date'=>$_POST['submitted_date'],
-        'status'=>$_POST['status']
-          
-      
+        'status'=>$_POST['status'],
+        'status'=>$my['user_id']
         ];
     $k = $db->insert('requests', $data);
     var_dump($db->error());
+    var_dump($k);
    
     if(!$db->error() && $k) {
         $msg = 'requests added successful';
@@ -24,7 +25,7 @@ if(isset($_POST['house_details'])){
     else $msg = 'Error adding requests';
     //var_dump($db->error());
 }
-$requests = $db->select('requests')->order_by('requests_id', 'desc')->fetchAll();
+$requests = $db->select('requests')->where(['requester'=>$my['user_id']])->order_by('requests_id', 'desc')->fetchAll();
 $data = ['requests'=>$requests,'msg'=>$msg, 'status'=>$ok,'request_uri'=>$request];
 echo helper::find_template('requests', $data);
 
