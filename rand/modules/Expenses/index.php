39e2fa93023e9c1bfb66376e644aab17893086b8
Jusper Wendo <jasperrupia@gmail.com>
2023-07-14 09:56:20 +0300

    wrong

--- rand/modules/Expenses/index.php
+++ rand/modules/Expenses/index.php
@@ -1,11 +1,11 @@
 <?php 
 
 $db = db::get_connection(storage::init()->system_config->database);
-$ok = false;
+$status = 'fail';
 $msg = '';
 
 $request = $_SERVER['REQUEST_URI'];
-if(isset($_POST['expenses_description'])){
+if(isset($_POST['add-expense'])){
     $data = [
         'expenses_date'=>$_POST['expenses_date'], 
         'expenses_description'=>$_POST['expenses_description'], 
@@ -13,20 +13,18 @@ if(isset($_POST['expenses_description'])){
        
        ];
     $k = $db->insert('expenses', $data);
-    //var_dump($db->error());
    
     if(!$db->error() && $k) {
-        $msg = 'expenses added successful';
-        $ok =true;
+        $msg = 'expense added successful';
+        $status = 'success';
     }
     else $msg = 'Error adding expenses';
-   //var_dump($db->error());
 }
 $expenses = $db->select('expenses')->order_by('expenses_id', 'desc')->fetchAll();
 $data = [
     'expenses'=>$expenses,
     'msg'=>$msg, 
-    'status'=>$ok,
+    'status'=>$status,
     'request_uri'=>$request,
     'currency'=>$storage->system_config->system_currency,
 ];
