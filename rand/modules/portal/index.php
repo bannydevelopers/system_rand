607d90f6971f9baa7af87473ee0b65bb6ea8fcc2
Ricky Graphics <rickygraphics001@gmail.com>
2023-09-04 15:22:22 +0300

    test

--- rand/modules/portal/index.php
+++ rand/modules/portal/index.php
@@ -184,15 +184,10 @@ if(isset($_GET['book'])){
 
 if(isset($_POST['book-an-apar'])){
     if(strtotime($_POST['checkout']) > strtotime($_POST['checkin'])){
-        $check_in = helper::format_time($_POST['checkin'],'Y-m-d H:i:s');
-        $whr = "apartment_category = '{$_POST['apartment_category']}' AND apartment_id NOT IN (SELECT apartment_reference FROM check_scheduling WHERE check_out < '{$check_in}')";
+        $whr = "apartment_category = '{$_POST['apartment_category']}' AND apartment_id NOT IN (SELECT apartment_reference FROM check_scheduling WHERE check_out < '{$_POST['checkin']}')";
         $available = $db->select('apartments', 'apartment_id')
-                        ->where($whr)->fetchAll();
-        $availableCount = $db->select('apartments', 'COUNT(apartment_id) AS availableCount')
-                        ->where($whr)->fetchAll();
-        print_r($availableCount[0]['availableCount']);
-        print_r($available[0]);
-        print_r($check_in);
+                        ->where($whr)
+                        ->fetchAll();
         if($available){
             shuffle($available);
             // insert into check_schedule
@@ -205,7 +200,7 @@ if(isset($_POST['book-an-apar'])){
                 'payment_amount' => $_POST['order_amount']
             ];
             $check_schedule = $db->insert('check_scheduling', $schedule);
-            // var_dump($db->error());
+            //var_dump($db->error());
             if(!$db->error() && $check_schedule){
                 $order = [
                     'special_requests'=>$_POST['special_request'], 
